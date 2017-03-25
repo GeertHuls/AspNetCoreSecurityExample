ae23aaa1e4b4e588b8afd752c70d2a07a81b05c4
Geert Huls <geert.huls82@gmail.com>
2017-03-25 21:47:59 +0100

    Removed redundant code

--- AspNetSecurity/Services/ConfArchUserClaimsPrincipalFactory.cs
+++ AspNetSecurity/Services/ConfArchUserClaimsPrincipalFactory.cs
@@ -9,25 +9,6 @@ using System.Linq;
 
 namespace AspNetSecurity.Services
 {
-    public class TopSecurityStampBasedTokenProvider<TUser> : IUserTwoFactorTokenProvider<TUser>
-        where TUser : class
-    {
-        public Task<string> GenerateAsync(string purpose, UserManager<TUser> manager, TUser user)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public Task<bool> ValidateAsync(string purpose, string token, UserManager<TUser> manager, TUser user)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public Task<bool> CanGenerateTwoFactorTokenAsync(UserManager<TUser> manager, TUser user)
-        {
-            throw new System.NotImplementedException();
-        }
-    }
-
     public class ConfArchUserClaimsPrincipalFactory :
         UserClaimsPrincipalFactory<ConfArchUser, IdentityRole>
     {
